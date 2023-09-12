41d1276a101634f86cb763daa4257b3d2c54d37c
Maxwell Zinzilieta <mr.zinzilieta@gmail.com>
2023-09-12 13:23:49 -0500

    initial commit

---
+++ proxy.cpp
@@ -0,0 +1,24 @@
+/***********************************/
+// Maxwell R. Zinzilieta
+// CS 447 Project 1 Basic Security Proxy 
+// September 2023
+/***********************************/
+
+/* Basic Libraries */
+#include <iostream>
+#include <string>
+
+/* Unix Libraries */
+#include <pthread.h>
+#include <unistd.h>
+
+/* Networking Libraries*/
+#include  <arpa/inet.h>
+#include <sys/socket.h>
+
+/* Defines */
+#define PORT 8080
+
+int main() {
+
+}
